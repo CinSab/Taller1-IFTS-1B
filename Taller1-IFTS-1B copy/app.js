60485ea7045737d810e6577853a576f5d9faf966
CinSab <73029945+CinSab@users.noreply.github.com>
2021-12-15 20:37:39 -0300

    ver 2

---
+++ Taller1-IFTS-1B copy/app.js
@@ -0,0 +1,149 @@
+const productos = [
+  //Dulces
+  { name: 'Afajor Chocolate', price: 60, type: "dulce", img: "./deco/alfajores.png"},
+  { name: 'Afajor Chocolate Blanco', price: 60, type: "dulce" , img: "./deco/alfajores.png"},
+  { name: 'Afajor Glaseado', price: 60,type: "dulce" , img: "./deco/alfajores.png"},
+  { name: 'Afajor Bon o Bon', price: 70,type: "dulce" , img: "./deco/alfajores.png"},
+  { name: 'Pan Dulce Frutos Secos', price: 300,type: "dulce" , img: "./deco/alfajores.png"},
+  { name: 'Pan Dulce Chocolate', price: 270,type: "dulce" , img: "./deco/alfajores.png"},
+  { name: 'Pan Dulce Solo', price: 250,type: "dulce" , img: "./deco/alfajores.png"},
+  { name: 'Pastaflora Dulce de Batata', price: 250,type: "dulce" , img: "./deco/alfajores.png"},
+  { name: 'Budin Limon', price: 200,type: "dulce" , img: "./deco/alfajores.png"},
+  { name: 'Budin Naranja', price: 200,type: "dulce" , img: "./deco/alfajores.png"},
+  { name: 'Budin vainilla', price: 200,type: "dulce" , img: "./deco/alfajores.png"},
+  { name: 'Budin Limon Chocolate', price: 220,type: "dulce" , img: "./deco/alfajores.png"},
+  //Salados
+  { name: 'Pan de Molde', price: 300,type: "salado", img: "./deco/alfajores.png" },
+  { name: 'Pan de Hamborgueso', price: 150,type: "salado" , img: "./deco/alfajores.png"},
+  { name: 'Pizzetas x 4', price: 400,type: "salado", img: "./deco/alfajores.png" },
+  { name: 'Empanadas a pedido - consultar precio', price: null,type: "salado", img: "./deco/alfajores.png" }
+]
+
+let dulcesHTML = " ";
+let saladosHTML = " ";
+const dataJsonProducts = './data/products.json';
+
+const bars = document.querySelector('.fa-bars');
+const sidebar = document.querySelector('.sidebar');
+const closingButton = document.querySelector('.fa-times-circle');
+const menu = document.querySelector('menu2');
+const dulce = document.getElementById('dulce');
+const salados = document.getElementById('sala3');
+const buttons = document.querySelectorAll("button");
+const modal = document.querySelector(".modal");
+const optionList = document.querySelectorAll("li");
+//const isVisible = "is-visible";
+
+buttons[1].addEventListener('click', function(){
+  modal.style.display = 'block';
+});
+
+buttons[0].addEventListener('click', function(){
+  modal.style.display = 'none';
+});
+
+
+
+bars.addEventListener('click', () => {
+    sidebar.classList.toggle('show-sidebar');
+});
+
+closingButton.addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
+});
+
+/*if(){
+  for(let i = 0; i < optionList.length; i++){
+      optionList[i].addEventListener('click', () => {
+        if(optionList[i] == 4 || optionList[i] == 5||optionList[i] == 6||optionList[i] == 7){
+          sidebar.classList.remove('show-sidebar');
+          console.log(optionList);
+      }
+      });
+}*/
+
+
+optionList[4].addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
+  console.log(optionList);
+});
+optionList[5].addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
+  console.log(optionList);
+});
+optionList[6].addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
+  console.log(optionList);
+});
+optionList[7].addEventListener('click', () => {
+  sidebar.classList.remove('show-sidebar');
+  console.log(optionList);
+});
+
+
+/*productos.forEach((item) =>
+  {
+    if(productos[item].type == "salado"){
+      saladosHTML +=
+        `<div>${productos[item].name} $${productos[item].price}</div>`;
+        salados.innerHTML = saladosHTML;
+    }else{
+      dulcesHTML +=
+        `<div class = "caca"> ${productos[i].name} $${productos[i].price}</div>`;
+        dulce.innerHTML = dulcesHTML;
+        }
+});*/
+
+/*fetch(dataJsonProducts)
+    .then(function(resp) {
+        return resp.JSON();
+    })
+    .then(function(data){
+      console.log(data);
+    })
+    .catch((err) => {
+  console.error('Error:', err);
+});*/
+for(let i in productos)
+{
+  if(productos[i].type == "salado"){
+    saladosHTML +=
+      `<div>${productos[i].name} $${productos[i].price}</div>`;
+      salados.innerHTML = saladosHTML;
+  }else{
+    dulcesHTML +=
+      `<div class = "caca"> ${productos[i].name} $${productos[i].price} </div>
+    <img class='imag'src="${productos[i].img}">`;
+      dulce.innerHTML = dulcesHTML;
+      }
+}
+
+
+/*for(let i = 0; i < productos.length; i++){
+  if(productos[i].type == "salado"){
+    saladosHTML +=
+      `<div>${productos[i].name} $${productos[i].price}</div>`;
+      salados.innerHTML = saladosHTML;
+  }else{
+    dulcesHTML +=
+      `<div class = "caca"> ${productos[i].name} $${productos[i].price}</div>`;
+      dulce.innerHTML = dulcesHTML;
+  }
+};
+for(let i = 0; i < productos.length; i++){
+  if(productos[i].type == "dulce"){
+    prodsHTML +=
+      '<div>'+ productos[i].name +'</div>'+
+      '<div>'+ '$' + productos[i].price +'</div>';
+
+  }*/
+
+
+
+
+
+/*fetch('./data/products.js')
+  .then(response => {
+    return response.json();
+  })
+  .then(data => console.log(data));*/
